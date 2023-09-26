c63faef62dfc06aa92d6a9a769a1ef924516866e
Samuel Boyden <sam@stboyden.com>
2023-09-26 11:43:57 +0100

    Start basic navigation menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,14 @@ import Link from "next/link";
 import { api } from "@/utils/api";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/ui/theme-mode-toggle";
+import { Separator } from "@/components/ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -25,6 +33,28 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <ModeToggle />
+        <div className="mx-auto flex min-w-full flex-row items-center p-2 last:justify-end">
+          <h1 className="m-0 text-2xl">
+            <Link href="/">Samuel Boyden</Link>
+          </h1>
+          <Separator orientation="vertical" className="mx-2 min-h-full" />
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+
+          {/* <div className="">
+            <ModeToggle />
+          </div> */}
+        </div>
+        <Separator />
         <main className="flex min-h-screen flex-col items-center justify-center"></main>
       </>
     </ThemeProvider>
