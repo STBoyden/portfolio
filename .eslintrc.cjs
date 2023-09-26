c37b423ae6810a7e2b973f748c4e768d2811a6be
Samuel Boyden <sam@stboyden.com>
2023-09-26 11:43:00 +0100

    Fix build
    Caused by a semver breakage by the radix-ui package used upstream by shadcn-ui.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,7 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
+  ignorePatterns: ["src/components/ui/*.tsx"]
 };
 
 module.exports = config;
