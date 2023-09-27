ca7f8c1bc1192d4bc0e2f9b253dcc6ca69403d25
Samuel Boyden <sam@stboyden.com>
2023-09-27 09:50:52 +0100

    Add warning for unused imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-esling/no-unused-imports": ["warn"]
   },
   ignorePatterns: ["src/components/ui/*.tsx"]
 };
