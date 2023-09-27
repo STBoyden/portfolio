9707fffddb05c44ae35c0edb7599a8dd744baf3b
Samuel Boyden <sam@stboyden.com>
2023-09-27 09:54:56 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-esling/no-unused-imports": ["warn"]
+    "@typescript-eslint/no-unused-imports": ["warn"]
   },
   ignorePatterns: ["src/components/ui/*.tsx"]
 };
