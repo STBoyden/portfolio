dc33fa63c23de861236aebe5a755ae3d3a3971e5
Samuel Boyden <sam@stboyden.com>
2023-10-05 21:44:24 +0100

    Fixed wording in first para, add more information about software dev hobby

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function AboutMe() {
   return (
@@ -15,7 +16,7 @@ export default function AboutMe() {
         <meta name="description" content="About Samuel Boyden" />
       </Head>
 
-      <span className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
+      <span className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl p-2">
         <Typography.Header1>Hi, my name is Sam!</Typography.Header1>
         <Typography.Paragraph>
           I am an aspiring software developer, currently studying a BSc Computer
@@ -27,7 +28,7 @@ export default function AboutMe() {
 
         <Typography.Header2>Hobbies</Typography.Header2>
 
-        <Accordion type="single" defaultValue="hobbies-software-development">
+        <Accordion type="multiple">
           <AccordionItem value="hobbies-software-development">
             <AccordionTrigger>
               <Typography.Header3>Software Development</Typography.Header3>
@@ -42,9 +43,37 @@ export default function AboutMe() {
                 {'"'} adventure games. This eventually lead me to join a school
                 that specialises in teaching computer science and engineering,
                 where I eventually learned C# as my first {'"'}real{'"'}{" "}
-                lanuage, then Python. And in my free time, I learnt how to make
+                lanuage, then Python. In my free time, I learnt how to make
                 websites with JavaScript, HTML and CSS, and how to make
                 low-level applications with C++.
+              </Typography.Paragraph>
+              <Typography.Paragraph>
+                Before I started university, I took my time to learn Rust - and
+                it soon became my primary language of choice to make anything.
+                The vast majority of my GitHub projects in the recent years have
+                utilised Rust - mainly back-end web servers.
+              </Typography.Paragraph>
+              <Typography.Paragraph>
+                During my first year in university, I joined a startup with a
+                friend of mine on the same course, focused on cyber-security. In
+                this new startup, I was responsible for two things: developing
+                tools for the team, and managing and hosting servers. The
+                primary tool that I was in charge of developing was called
+                CodeCTRL, which was a utility for code debugging, that would
+                receive logs from clients in various languages and help the
+                traversal and navigation of stracktraces produced during
+                runtime. Unfortunately, the company disbanded at the end of my
+                second year, but CodeCTRL is now under my account:{" "}
+                <Link
+                  href="https://github.com/STBoyden/codectrl"
+                  className="underline"
+                >
+                  here
+                </Link>
+                . Due to the nature of this project, it resulted in me learning
+                and implementing gRPC services, and writing in new languages
+                such as Go - which has since become a staple in the languages
+                that I commonly use.
               </Typography.Paragraph>
             </AccordionContent>
           </AccordionItem>
@@ -73,6 +102,7 @@ export default function AboutMe() {
                 time, it was still a huge upgrade from the FujiFilm - this time
                 actually allowing me to swap lenses!
               </Typography.Paragraph>
+              {/* TODO: More about photography */}
             </AccordionContent>
           </AccordionItem>
         </Accordion>
