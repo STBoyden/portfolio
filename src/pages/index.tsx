36c98375d8e68744c72f33f3b08a23a5888e1050
Samuel Boyden <sam@stboyden.com>
2025-03-12 17:41:27 +0000

    Update index.tsx
    yer - fix that shit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,10 +71,12 @@ export default function Home() {
           )}
 
           <span className="lg:ml-2 lg:border-l-2 lg:pl-2">
-            <Typography.Paragraph className="italic">
-              Note: This site is a little outdated - last updated 2 years 
-              ago. I will be updating it soon to reflect latest changes
-              soon.
+            <Typography.Paragraph>
+              <div className="italic">
+                Note: This site is a little outdated - last updated 2 years 
+                ago. I will be updating it soon to reflect latest changes
+                soon.
+              </div>
             </Typography.Paragraph>
             <Typography.Paragraph>
               I am an aspiring software developer, currently studying a BSc
